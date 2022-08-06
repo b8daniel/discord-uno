0ec64f634f7b49ca572c73f4aa74c28765294172
Oxymoron <71148001+blxckOxymoron@users.noreply.github.com>
2022-08-06 23:21:45 +0200

    :sparkles: create example config

---
+++ src/ts/config.ts
@@ -0,0 +1,12 @@
+export const token = "SOMETEXT.ORNUMBERS.ORDASHES"; // bot token from developer dashboard
+export const clientId = "902616076196651058"; // bot client id
+export const devServerId = "747090837732589570"; // dev commands are only enabled on this server 
+export const ownerId = "634832620521259008"; // WIP to send error messages
+// info for an embed and the activity
+export const sponsor = "UserBot"; 
+export const sponsorInvite = "https://discord.gg/2vPc9NNRgZ";
+export const sponsorDescription = "🛈 Cooler Minecraft Bot";
+export const botInvite = // TODO generate from client
+  "https://discord.com/oauth2/authorize?client_id=902616076196651058&scope=bot%20applications.commands&permissions=326686010368";
+
+export const notifyRoleId = "926918916872220672"; // role to get notified when a game starts
